2b12e735f5d06570659d6b49e2531cc978d90d2e
AllyVA <alexandraalexova12@gmail.com>
2026-01-29 14:52:44 +0200

    FR3

--- src/model/DeliveryRoute.java
+++ src/model/DeliveryRoute.java
@@ -2,15 +2,20 @@ package model;
 
 import constants.Location;
 import java.util.List;
+import java.util.ArrayList;
 
 public class DeliveryRoute {
 
     private final String id;
     private final List<Location> locations;
 
+    private String departureTime;
+    private final List<String> arrivalTimes;
+
     public DeliveryRoute(String id, List<Location> locations) {
         this.id = id;
         this.locations = locations;
+        this.arrivalTimes = new ArrayList<>();
     }
 
     public String getId() {
@@ -19,5 +24,21 @@ public class DeliveryRoute {
 
     public List<Location> getLocations() {
         return locations;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public List<String> getArrivalTimes() {
+        return arrivalTimes;
+    }
+
+    public void addArrivalTime(String arrivalTime) {
+        arrivalTimes.add(arrivalTime);
     }
 }
